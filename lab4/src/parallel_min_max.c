75a56c351d4adc53e6ccb060d183401b41caff0c
parrrish-1 <mvkabir@mail.ru>
2020-05-06 22:10:52 +0000

    changed

--- lab4/src/parallel_min_max.c
+++ lab4/src/parallel_min_max.c
@@ -5,30 +5,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <signal.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <getopt.h>
-
 #include "find_min_max.h"
 #include "utils.h"
+
+
+volatile pid_t* child_array;
+volatile int child_number;
+volatile int pnum;
+
+void Htime(int i){
+
+printf("Время вышло! Было создано %d дочерних процессов \n",child_number );
+
+pnum=child_number;
+
+for(int i=0; i<child_number; i++)
+{
+
+kill(child_array[i],SIGKILL);
+
+}
+free(child_array);
+
+}
+
+
+
 
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
-  int pnum = -1;
+
   bool with_files = false;
-  
- 
-  while (true) {
+  int timeout=0;
+  pnum = -1;
+
+
+while (true) {
     int current_optind = optind ? optind : 1;
 
     static struct option options[] = {{"seed", required_argument, 0, 0},
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
                                       {"by_files", no_argument, 0, 'f'},
+				      {"timeout", required_argument, 0, 0},
                                       {0, 0, 0, 0}};
 
     int option_index = 0;
@@ -41,33 +66,36 @@ int main(int argc, char **argv) {
           case 0:
             seed = atoi(optarg);
             // your code here
-            // error handling
-            if(seed <= 0){
-            printf("seed is positive\n");		
-            return -1;
+		if(seed<=0){
+		   printf("seed can't be negative \n");		
+		   return -1;
 		}
+            // error handling
             break;
           case 1:
             array_size = atoi(optarg);
             // your code here
-            // error handling
-            if(array_size <= 0){
-            printf("array_size is positive \n");		
-            return -1;
+		if(array_size<=0){
+		   printf("size can't be negative \n");		
+		   return -1;
 		}
+            // error handling
             break;
           case 2:
             pnum = atoi(optarg);
+		if(pnum<=0){
+		   printf("amount of processes can't be negative\n");		
+		   return -1;
+		}
             // your code here
             // error handling
-            if(pnum <= 0){
-            printf("amount of processes is positive\n");		
-            return -1;
-		}
             break;
           case 3:
             with_files = true;
             break;
+	  case 4:
+	    timeout=atoi(optarg);
+	    break;
 
           default:
             printf("Index %d is out of options\n", option_index);
@@ -85,7 +113,7 @@ int main(int argc, char **argv) {
         printf("getopt returned character code 0%o?\n", c);
     }
   }
-	
+
 
   if (optind < argc) {
     printf("Has at least one no option argument\n");
@@ -97,19 +125,14 @@ int main(int argc, char **argv) {
            argv[0]);
     return 1;
   }
+
   int *array = (int*)malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
-  int i = 0;
-  for (i = 0; i < array_size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
   int active_child_processes = 0;
   int f_p[2];
   int sizeforprocess= pnum<=array_size ? (array_size/pnum) : 1;
+
   FILE *file;
-  int j = 0;
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
   if(!with_files){  
@@ -117,39 +140,47 @@ int main(int argc, char **argv) {
         	printf("Can\'t create pipe\n");
 	      }
   }else{
-	
-  	file=fopen("file","wb+");
-	if(file == NULL)
+
+  	file=fopen("tile","wb+");
+	if(file==NULL)
 		printf("Can\'t create file\n");
 	}
-  for (i = 0; i < pnum; i++) {
 
-    pid_t child_pid = fork();
-    if (child_pid >= 0) {
+
+child_array=(pid_t*)malloc(pnum*sizeof(pid_t));
+
+  alarm(timeout);
+  signal(SIGALRM,&Htime);
+
+
+  for (int i = 0; i < pnum; i++) {
+
+    child_array[i] = fork();
+    if (child_array[i] >= 0) {
       // successful fork
       active_child_processes += 1;
-
-      if (child_pid == 0) {
+      child_number= active_child_processes;
+      if (child_array[i] == 0) {
 	// child process
         // parallel somehow
-    int Min_Max[2];	  
+        int Min_Max[2];	  
 	int num=0;
 	int min = INT_MAX;
-    int max = INT_MIN;
-	int  begin = sizeforprocess*(active_child_processes-1);
-	int end = begin + sizeforprocess;
+    	int max = INT_MIN;
+	int  begin=sizeforprocess*(active_child_processes-1);
+	int end=begin+sizeforprocess;
 	if(active_child_processes==pnum) 
 	    end=array_size;
-	     
-	for(j = begin; j < end; j++){
+
+	for(int j=begin;j<end;j++){
 		num=array[j];
 	        if(num<min) min=num;
 	        if(num>max) max=num;
 	     }
 	Min_Max[0]=min;
 	Min_Max[1]=max;
-    //printf("Process[%03d]:\t%d\t%d\n", active_child_processes, min, max);
-    if (with_files) {
+        if (with_files) {
+
           // use files here
 	fwrite(Min_Max,1,sizeof(Min_Max),file);
 	fclose(file);
@@ -167,40 +198,40 @@ int main(int argc, char **argv) {
       return 1;
     }
   }
- 
- int* stat;
+
   while (active_child_processes > 0) {
-   
-    wait(stat);
+    wait(0);
     active_child_processes -= 1;
   }
+
 if(!with_files)
  close(f_p[1]);
 else{
 fclose(file);
-  file=fopen("file","rb");
-if(file==NULL)
+  file=fopen("tile","rb");
+ if(file==NULL)
 	printf("Can\'t create file\n");
 }
- 
+
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  for (i = 0; i < pnum; i++) {
+  for (int i = 0; i < pnum; i++) {
     int min = INT_MAX;
     int max = INT_MIN;
-    int arrmm[2];
-	
+    int mmnumb[2];
+
     if (with_files) {
        //read from files
-	fread(arrmm,1,sizeof(arrmm),file);
+	fread(mmnumb,1,sizeof(mmnumb),file);
     } else {
       // read from pipes
-	read(f_p[0],arrmm,sizeof(int)*2);
+	read(f_p[0],mmnumb,sizeof(int)*2);
     }
-	min=arrmm[0];
-	max=arrmm[1];	
+	min=mmnumb[0];
+	max=mmnumb[1];	
+
     if (min < min_max.min) min_max.min = min;
     if (max > min_max.max) min_max.max = max;
   }
